00ce704b79c8d6e03b481913d96855d3029219d7
minbeom <asdf961008@gmail.com>
2025-08-31 02:28:55 +0900

    feat: #15 모바일 추가
    close #15

---
+++ Client/Assets/@Scripts/State/Contents/Tetromino/IdleState.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : abState
+{
+    public IdleState(StateLists stateLists, int stateId)
+        : base(stateLists, stateId)
+    {
+    }
+
+    public override void OnEnter()
+    {
+        Managers.UI.ShowPopupUI<UI_GamePopup>();
+    }
+
+    public override void OnLeave()
+    {
+        Managers.UI.ClosePopupUI();
+        Managers.Scene.LoadScene(Define.EScene.SingleGameScene);
+    }
+
+    public override void OnProcEveryFrame()
+    {
+        UI_Popup popup = Managers.UI.GetPopupUI("UI_GamePopup");
+        if (popup != null && popup.gameObject.activeSelf)
+        {
+            if (IsClicked())
+            {
+                StateComponents stateComponents = GetStateComponents();
+                stateComponents.ChangeState((int)E_TETROMINO_STATE.Falling);
+            }
+        }
+    }
+
+    public override void OnProcOnce()
+    {
+    }
+
+    private bool IsClicked()
+    {
+        // 모바일 클릭체크
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            return true;
+
+        // PC esc 누름체크
+        if (Input.GetKeyDown(KeyCode.Escape))
+            return true;
+
+        return false;
+    }
+}
--- Client/Assets/@Scripts/State/Contents/Tetromino/TetrominoStateLists.cs
+++ Client/Assets/@Scripts/State/Contents/Tetromino/TetrominoStateLists.cs
@@ -9,8 +9,11 @@ public class TetrominoStateLists : StateLists
     {
         stateList = new Dictionary<int, abState>
         {
+            { (int)E_TETROMINO_STATE.Idle, new IdleState(this, (int)E_TETROMINO_STATE.Idle) },
             { (int)E_TETROMINO_STATE.Falling, new FallingState(this, (int)E_TETROMINO_STATE.Falling) },
-            { (int)E_TETROMINO_STATE.Moving, new MovingState(this, (int)E_TETROMINO_STATE.Moving) },
+            { (int)E_TETROMINO_STATE.DownMoving, new DownMovingState(this, (int)E_TETROMINO_STATE.DownMoving) },
+            { (int)E_TETROMINO_STATE.LeftMoving, new LeftMovingState(this, (int)E_TETROMINO_STATE.LeftMoving) },
+            { (int)E_TETROMINO_STATE.RightMoving, new RightMovingState(this, (int)E_TETROMINO_STATE.RightMoving) },
             { (int)E_TETROMINO_STATE.Rotating, new RotatingState(this, (int)E_TETROMINO_STATE.Rotating) },
             { (int)E_TETROMINO_STATE.Dropping, new DroppingState(this, (int)E_TETROMINO_STATE.Dropping) },
             { (int)E_TETROMINO_STATE.Locked, new LockedState(this, (int)E_TETROMINO_STATE.Locked) },
