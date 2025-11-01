25a01b51115c1dcb6c33428c18533adbee9755aa
minbeom <asdf961008@gmail.com>
2025-11-01 21:28:02 +0900

    feat: #63 EntryPoint.sh 수정
    close #63

--- Server/GameServer/EntryPoint.sh
+++ Server/GameServer/EntryPoint.sh
@@ -2,7 +2,7 @@
 
 IP=$(hostname -I | awk '{print $1}')
 
-sed -i "s/127.0.0.1/${IP}/g" /App/config.json
-sed -i "s/152.67.197.127/infra-node-mariadb-mariadb-1/g" /App/config.json
+sed -i "s/127.0.0.1/${IP}/g" /App/Server/GameServer/config.json
+sed -i "s/152.67.197.127/infra-node-mariadb-mariadb-1/g" /App/Server/GameServer/config.json
 
 exec "$@"
